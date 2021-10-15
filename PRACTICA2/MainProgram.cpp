27d6216b9dc4a617e340f8132f0890c5eefe7f76
p72losaj <55488505+p72losaj@users.noreply.github.com>
2021-10-15 08:53:49 +0200

    ANADIDOS ARCHIVOS INICIALES PRACTICA2

---
+++ PRACTICA2/MainProgram.cpp
@@ -0,0 +1,45 @@
+/*
+    Programa principal
+    Realizado por Jaime Lorenzo Sanchez
+*/
+
+// Biblioteca MainProgram
+
+#include "MainProgram.hpp"
+
+/**
+ * Funcion principal del programa
+*/
+
+int main(){
+    
+    // Inicializador de la semilla aleatoria
+    
+    srand(time(NULL)); 
+    // Variable que gestiona la operacion que desee realizar el usuario
+
+    int opcion = 1;
+    /*
+        Mostramos un menu con las siguientes opciones
+        1- void ordenacionQuickSort()
+        2- void ordenacionFusion()
+    */
+    while(opcion != 0){
+        std::cout << "MENU DE OPERACIONES" << endl;
+        std::cout << "1- void ordenacionQuickSort() " << endl;
+        std::cout << "2- void ordenacionFusion() " << endl;
+        std::cout << "0. Salir " << endl;
+        std::cout << "Elige una opcion " << endl;
+        cin >> opcion;
+        switch(opcion){
+        case 1:
+            //ordenacionQuickSort();
+        break;
+        case 2:
+            //ordenacionFusion();
+        break;
+        }
+    }
+
+   
+}
