b28fba5cb53c8a0ee9f637068a32a4d26442dad4
p72losaj <p72losaj@uco.es>
2021-11-25 08:38:45 +0100

    Fichero makelist

---
+++ PRACTICA1/PROPIO/build/CMakeFiles/practica1.exe.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jaime/Escritorio/UCO/PROPIO/ALGORITMICA21-22/ALGORITMICA-2122/PRACTICA1/PROPIO/MainProgram.cpp" "/home/jaime/Escritorio/UCO/PROPIO/ALGORITMICA21-22/ALGORITMICA-2122/PRACTICA1/PROPIO/build/CMakeFiles/practica1.exe.dir/MainProgram.cpp.o"
+  "/home/jaime/Escritorio/UCO/PROPIO/ALGORITMICA21-22/ALGORITMICA-2122/PRACTICA1/PROPIO/fibonacci.cpp" "/home/jaime/Escritorio/UCO/PROPIO/ALGORITMICA21-22/ALGORITMICA-2122/PRACTICA1/PROPIO/build/CMakeFiles/practica1.exe.dir/fibonacci.cpp.o"
+  "/home/jaime/Escritorio/UCO/PROPIO/ALGORITMICA21-22/ALGORITMICA-2122/PRACTICA1/PROPIO/funcionesComunes.cpp" "/home/jaime/Escritorio/UCO/PROPIO/ALGORITMICA21-22/ALGORITMICA-2122/PRACTICA1/PROPIO/build/CMakeFiles/practica1.exe.dir/funcionesComunes.cpp.o"
+  "/home/jaime/Escritorio/UCO/PROPIO/ALGORITMICA21-22/ALGORITMICA-2122/PRACTICA1/PROPIO/ordenacionSeleccion.cpp" "/home/jaime/Escritorio/UCO/PROPIO/ALGORITMICA21-22/ALGORITMICA-2122/PRACTICA1/PROPIO/build/CMakeFiles/practica1.exe.dir/ordenacionSeleccion.cpp.o"
+  "/home/jaime/Escritorio/UCO/PROPIO/ALGORITMICA21-22/ALGORITMICA-2122/PRACTICA1/PROPIO/productoMatricesCuadradas.cpp" "/home/jaime/Escritorio/UCO/PROPIO/ALGORITMICA21-22/ALGORITMICA-2122/PRACTICA1/PROPIO/build/CMakeFiles/practica1.exe.dir/productoMatricesCuadradas.cpp.o"
+  "/home/jaime/Escritorio/UCO/PROPIO/ALGORITMICA21-22/ALGORITMICA-2122/PRACTICA1/PROPIO/sistemaEcuaciones.cpp" "/home/jaime/Escritorio/UCO/PROPIO/ALGORITMICA21-22/ALGORITMICA-2122/PRACTICA1/PROPIO/build/CMakeFiles/practica1.exe.dir/sistemaEcuaciones.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ PRACTICA1/PROPIO/build/CMakeFiles/practica1.exe.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practica1.exe.dir/MainProgram.cpp.o"
+  "CMakeFiles/practica1.exe.dir/fibonacci.cpp.o"
+  "CMakeFiles/practica1.exe.dir/funcionesComunes.cpp.o"
+  "CMakeFiles/practica1.exe.dir/ordenacionSeleccion.cpp.o"
+  "CMakeFiles/practica1.exe.dir/productoMatricesCuadradas.cpp.o"
+  "CMakeFiles/practica1.exe.dir/sistemaEcuaciones.cpp.o"
+  "practica1.exe"
+  "practica1.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practica1.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
